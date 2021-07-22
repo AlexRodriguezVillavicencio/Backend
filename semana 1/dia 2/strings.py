bca376357df7e9c3e61a20a4ab283b0c8db73d81
Alex <arodriguezv@uni.pe>
2021-07-21 21:11:13 -0500

    funcion eliminar

--- semana 1/dia 2/strings.py
+++ semana 1/dia 2/strings.py
@@ -1,21 +1,43 @@
-mystring = "hola mundo"
-print(mystring.upper())
-print(mystring.lower())
-print(mystring.swapcase())
-print(mystring.capitalize())
-print(mystring.replace('hola','adios'))
+# mystring = "hola mundo"
+# print(mystring.upper())
+# print(mystring.lower())
+# print(mystring.swapcase())
+# print(mystring.capitalize())
+# print(mystring.replace('hola','adios'))
+
 #CUENTA CUANTAS O HAY
-print(mystring.count('o'))
+# print(mystring.count('o'))
+
 #EL TEXTO EPIEZA CON:
-print(mystring.startswith('hola'))
+# print(mystring.startswith('hola'))
+
 #MI TEXTO TERMINA CON:
-print(mystring.endswith('mundo'))
+# print(mystring.endswith('mundo'))
 
 
-list = ['hl',20]
-tuple = ['hl',20]
-dictionary = {'name1':'hl', 'name2':20}
+# list = ['hl',20]
+# tuple = ['hl',20]
+# dictionary = {'name1':'hl', 'name2':20}
 
-print(len(list))
-print(len(tuple))
-print(len(dictionary))
+# print(len(list))
+# print(len(tuple))
+# print(len(dictionary))
+
+
+#CON LA PROPIEDAD DIR PUEDO VER QUE METODOS SE PUEDEN HACER:
+colors = ['red','blue','orange']
+print(dir(colors))
+#PARA ORDENAR LA LISTA:
+colors.sort()
+print(colors)
+#PARA ORDENAR A LA INVERSA:
+colors.sort(reverse = True)
+print(colors)
+
+#set
+colors = {1:'red',2:'blue',3:'orange'}
+print(type(colors))
+
+#diretory
+colors = {1:'red',2:'blue',3:'orange'}
+print(type(colors))
