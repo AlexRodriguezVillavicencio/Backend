c5a36ba34c980f5d70ec8827d15479aca6e20e55
Alex <arodriguezv@uni.pe>
2021-07-27 17:59:47 -0500

    dia 4

--- semana 1/dia 4/libEscuela.py
+++ semana 1/dia 4/libEscuela.py
@@ -1,3 +1,34 @@
+class clsPersona:
+
+    def __init__(self,nom,ema,cel):
+        self.nombre = nom
+        self.email = ema
+        self.celular = cel
+
+    def registrase():
+        pass
+
+class clsUsuario:
+    def __init__(self,user,password):
+        self.nombreUsuario = user
+        self.password = password    
+
+    def login():
+        pass
+
+
+class clsProfesor(clsPersona,clsUsuario):
+
+    def __init__(self,nom,ema,cel,esp,u,p):
+        clsPersona.__init__(self,nom,ema,cel)
+        clsUsuario.__init__(self,u,p)
+        self.especialidad = esp
+
+    def calificar(self):
+        pass
+
+
+
 class clsAlumno:
 
     def __init__(self,nom,ema,cel):
@@ -5,16 +36,15 @@ class clsAlumno:
         self.email = ema
         self.celular = cel
 
-    def create(self):
-        pass
-    
-    def read(self):
+    def enviarEjercicios():
         pass
 
-    def update(self):
-        pass
+class clsCurso:
 
-
+    def __init__(self,cod,nom,nota):
+        self.codigo = cod
+        self.nombre = nom
+        self.nota = nota
 
 
 
---
+++ semana 1/dia 4/main.py
@@ -0,0 +1,85 @@
+from libEscuela import clsAlumno
+import os
+#PROGRAMA PARA GESTÓN DE ALUMNOS
+#CRUD : CREATE , READ, UPDATE, DELETE
+#DEFINIR VARIABLES DE ENTRADA Y SALIDA
+alumnos = []
+alumno = {}
+salir = 'no'
+
+def menuopciones():
+    print("*" * 20)
+    print("[1] REGISTRAR ALUMNO")
+    print("[2] MOSTRAR ALUMNOS")
+    print("[3] ACTUALIZAR ALUMNO")
+    print("[4] ELIMINAR ALUMNO")
+    print("*" * 20)
+
+def cargarAlumnos(strAlumnos):
+    lstAlumnosData = []
+    lstAlumnos = strAlumnos.splitlines()
+    del lstAlumnos[0]
+    for objAlumno in lstAlumnos:
+        lstObjAlumno = objAlumno.split(',')
+        nombre = lstObjAlumno[0]
+        email = lstObjAlumno[1]
+        celular = lstObjAlumno[2]
+        nuevoAlumno = clsAlumno(nombre,email,celular)
+        lstAlumnosData.append(nuevoAlumno)
+    return lstAlumnosData
+
+def mostrarAlumnos():
+    print("====================================")
+    print("=====Listado de alumnos=============")
+    print("====================================")
+    for a in alumnos:
+        print(a.nombre + "|" + a.email + "|" + a.celular)
+        print("====================================")
+
+def grabarAlumnos():
+    strAlumnos = ""
+    for a in alumnos:
+        strAlumnos += "\n" + a.nombre + "," + a.email + "," + a.celular
+    return strAlumnos
+
+fileName = r"alumnos.txt"
+
+if (os.path.isfile(fileName)):
+    fr = open(fileName,'r')
+    fAlumnos = fr.read()
+    #print(fAlumnos)
+    alumnos = cargarAlumnos(fAlumnos)
+    fr.close()
+# else:
+#     fr = open(fileName,'w')
+#     fr.write("\n")
+#     alumnos[]
+#     fr.close()
+
+while(salir == 'no'):
+    menuopciones()
+    opcion = input("INGRESE OPCIÓN: ")
+    if(opcion == "1"):
+        print("REGISTRO DE NUEVO ALUMNO:")
+        nombre = input("NOMBRE : ")
+        email = input("EMAIL : ")
+        celular = input("CELULAR : ")
+        nAlumno = clsAlumno(nombre,email,celular)
+        alumnos.append(nAlumno)
+    elif(opcion == "2"):
+        mostrarAlumnos()
+    elif(opcion == "3"):
+        pass
+    elif(opcion == "4"):
+        pass
+    else:
+        print("MARCO UNA OPCIÓN INCORRECTA")
+        continue
+    print("Desea salir del programa? ")
+    salir = input()
+    if(salir == "si"):
+        print("ADIOS")
+        strAlumnosGrabar = grabarAlumnos()
+        fw = open(fileName,'w')
+        fw.write(strAlumnosGrabar)
+        fw.close()
